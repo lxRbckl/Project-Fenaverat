b46ec69e673c119dd04604a1fe8557297fa7279b
lxRbckl <lxrbckl@gmail.com>
2024-02-06 01:52:30 -0600

    file structure

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-# Project Fenaverat by Alex Arbuckle #
+# < Project Fenaverat 3 by Alex Arbuckle > #
 
 
 # import <
-from backend.resource import application, server
-from frontend.layout.template import templateFunction
+
 
 # >
 
 
-# set layout <
-application.layout = templateFunction()
-
-# >
-
-
-# main <
-if (__name__ == '__main__'): application.run_server(debug = True)
-
-# >
+if (__name__ == '__main__'):
+   
+   pass
